9e22ed72fda734e3ac5e010b7dab6c4f78945038
Edwin Ajahuanca <edwinajahuancacallisaya@gmail.com>
2017-11-30 19:21:02 -0400

    modelo user

--- app/User.php
+++ app/User.php
@@ -27,9 +27,6 @@ class User extends Authenticatable
                             'password',
                             'remember_token',
                         ];
-    public function movimientos(){
-        return $this->hasMany('App\Movimiento');
-    }
 
     public function cursos(){
         return $this->hasMany('App\Curso');
